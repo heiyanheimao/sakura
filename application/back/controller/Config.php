a9d175ca58633d072b27a4b05d8cffb973a6c8fa
heiyanheimao <550715038@qq.com>
2019-10-22 09:23:34 +0800

    111

--- application/back/controller/Config.php
+++ application/back/controller/Config.php
@@ -93,11 +93,11 @@ class Config extends Base
 
             //接收参数
             $input['config_id'] = $request->post('config_id');//配置id
-            $input['state']    = $request->post('state');//状态
+            $input['config_state']    = $request->post('config_state');//状态
             //参数校验
             $validate = new Validate();
             $validate->rule([
-                'state' => function ($v) {
+                'config_state' => function ($v) {
                     if (null === $v) {
                         return '缺少参数';
                     }
@@ -120,9 +120,189 @@ class Config extends Base
                 return jsonData(401, $validate->getError());
             }
             //逻辑处理
-            $model = new HotelModel();
+            $model = new ConfigModel();
             return $model->changeState($input);
         }
         return jsonData(400, '非法请求');
+    }
+
+    /**修改配置描述
+     * @param Request $request
+     * @return array
+     */
+    public function editDesc(Request $request)
+    {
+        if (self::checkRequest($request)) {
+            //登录态检测
+            if (!($info = self::getStatus())) {
+                return jsonData(500, '登录态失效，请重新登录');
+            }
+            //权限检测
+            $auth = self::checkAuth($info, $this->level);
+            if (200 != $auth['code']) {
+                return $auth;
+            }
+
+            //接收参数
+            $input['config_id'] = $request->post('config_id');//配置id
+            $input['config_desc']    = $request->post('config_desc');//状态
+            //参数校验
+            $validate = new Validate();
+            $validate->rule([
+                'config_desc' => function ($v) {
+                    if (null === $v) {
+                        return '缺少参数';
+                    }
+                    return true;
+                },
+                'config_id' => function ($v) {
+                    if (null === $v) {
+                        return '缺少参数';
+                    }
+                    if (false == isPosInt($v)) {
+                        return '不合法的参数';
+                    }
+                    return true;
+                }
+            ]);
+            if (!$validate->check($input)) {
+                return jsonData(401, $validate->getError());
+            }
+            //逻辑处理
+            $model = new ConfigModel();
+            return $model->editDesc($input);
+        }
+        return jsonData(400, '非法请求');
+    }
+
+    /**修改配置值
+     * @param Request $request
+     * @return array
+     */
+    public function editValue(Request $request)
+    {
+        if (self::checkRequest($request)) {
+            //登录态检测
+            if (!($info = self::getStatus())) {
+                return jsonData(500, '登录态失效，请重新登录');
+            }
+            //权限检测
+            $auth = self::checkAuth($info, $this->level);
+            if (200 != $auth['code']) {
+                return $auth;
+            }
+
+            //接收参数
+            $input['config_id'] = $request->post('config_id');//配置id
+            $input['config_value']    = $request->post('config_value');//状态
+            $input['type']    = $request->post('type');//状态
+            //参数校验
+            $validate = new Validate();
+            $validate->rule([
+                'config_value' => function ($v, $all) {
+                    if (null === $v) {
+                        return '缺少参数';
+                    }
+                    if ($all['type'] == 'time' && preg_match('/^(20|21|22|23|[0-1]\d):[0-5]\d:[0-5]\d$/', $v) == 0) {
+                        return '不合法的参数';
+                    }
+                    return true;
+                },
+                'config_id' => function ($v) {
+                    if (null === $v) {
+                        return '缺少参数';
+                    }
+                    if (false == isPosInt($v)) {
+                        return '不合法的参数';
+                    }
+                    return true;
+                },
+                'type' => function ($v) {
+                    if (null === $v) {
+                        return '缺少参数';
+                    }
+                    if (!in_array($v, ['string', 'time'], true)) {
+                        return '不合法的参数';
+                    }
+                    return true;
+                },
+            ]);
+            if (!$validate->check($input)) {
+                return jsonData(401, $validate->getError());
+            }
+            //逻辑处理
+            unset($input['type']);
+            $model = new ConfigModel();
+            return $model->editValue($input);
+        }
+        return jsonData(400, '非法请求');
+    }
+
+    /**上传icon
+     * @param Request $request
+     * @return array|bool|false|mixed|\think\File
+     */
+    public function uploadIcon(Request $request)
+    {
+        //登录态检测
+        if (!($info = self::getStatus())) {
+            return jsonData(500, '登录态失效，请重新登录');
+        }
+        //权限检测
+        $auth = self::checkAuth($info, $this->level);
+        if (200 != $auth['code']) {
+            return $auth;
+        }
+
+        //接收参数
+        $input['config_id'] = $request->post('config_id');//配置id
+        //参数校验
+        $validate = new Validate();
+        $validate->rule([
+            'config_id' => function ($v) {
+                if (null === $v) {
+                    return '缺少参数';
+                }
+                if (false == isPosInt($v)) {
+                    return '不合法的参数';
+                }
+                return true;
+            }
+        ]);
+        if (!$validate->check($input)) {
+            return jsonData(401, $validate->getError());
+        }
+        //获取配置
+        $config = (new ConfigModel())->getConfig(['IMG_MAX', 'IMG_TYPE']);
+        if (false == $config) {
+            return jsonData(401, '未获取到配置项');
+        }
+        //获取文章原始封面
+        $model     = new ConfigModel();
+        $coverInfo = $model->getIcon($input);
+        if (200 != $coverInfo['code']) {
+            return $info;
+        }
+        //上传
+        $file = $request->file('file');
+        $info = $file->validate([
+            'size' => $config[0]['config_value'] * 1024,
+            'ext' => $config[1]['config_value']
+        ])->move(config('app.upload_path') . 'icon/');
+        if ($info) {
+            //保存到数据库
+            if ($model->updateIcon($input, '/uploads/icon/' . $info->getSaveName())) {
+                if ($coverInfo['data']['config_value'] != '') {
+                    @unlink(config('app.static_path') . $coverInfo['data']['config_value']);
+                }
+                return jsonData(200, '上传成功', ['url' => '/uploads/icon/' . $info->getSaveName()]);
+            } else {
+                @unlink(config('app.upload_path') . 'icon/' . $info->getSaveName());
+                return jsonData(403, '上传失败', []);
+            }
+        } else {
+            // 上传失败获取错误信息
+            return jsonData(402, $file->getError());
+        }
     }
 }
--- application/back/model/ConfigModel.php
+++ application/back/model/ConfigModel.php
@@ -10,6 +10,7 @@ namespace app\back\model;
 
 
 use think\Db;
+use think\facade\Session;
 
 class ConfigModel
 {
@@ -84,6 +85,141 @@ class ConfigModel
         } catch (\Exception $e) {
 //            var_dump($e->getMessage());
             return jsonData(301, '服务内部错误~');
+        }
+    }
+
+    /**修改配置状态
+     * @param $input
+     * @return array
+     */
+    public function changeState($input)
+    {
+        $guy = Session::get('info.admin_account');
+        try {
+            //查询配置当前状态
+            $info = Db::table($this->table)->field('config_state')->where('config_id', $input['config_id'])->find();
+            if (null == $info) {
+                return jsonData(401, '未找到当前配置');
+            }
+            if ($input['config_state'] == $info['config_state']) {
+                return jsonData(402, '修改状态与当前状态一致，不能修改');
+            }
+            $rtn = Db::table($this->table)->limit(1)->update($input);
+            if ($rtn == 1) {
+                AdminLogModel::writeLog($guy, '修改配置状态', json_encode($input), '修改成功');
+                return jsonData(200, '状态修改成功');
+            }
+            AdminLogModel::writeLog($guy, '修改配置状态', json_encode($input), '修改失败');
+            return jsonData(403, '状态修改失败');
+        } catch (\Exception $e) {
+//            var_dump($e->getMessage());
+            AdminLogModel::writeLog($guy, '修改配置状态', json_encode($input), '服务内部错误~');
+            return jsonData(301, '服务内部错误~');
+        }
+    }
+
+    /**修改配置描述
+     * @param $input
+     * @return array
+     */
+    public function editDesc($input)
+    {
+        $guy = Session::get('info.admin_account');
+        try {
+            //查询配置当前状态
+            $info = Db::table($this->table)->field('config_desc')->where('config_id', $input['config_id'])->find();
+            if (null == $info) {
+                return jsonData(401, '未找到当前配置');
+            }
+            if ($input['config_desc'] == $info['config_desc']) {
+                return jsonData(402, '修改前与当前一致，不能修改');
+            }
+            $rtn = Db::table($this->table)->limit(1)->update($input);
+            if ($rtn == 1) {
+                AdminLogModel::writeLog($guy, '修改配置描述', json_encode($input), '修改成功');
+                return jsonData(200, '描述修改成功');
+            }
+            AdminLogModel::writeLog($guy, '修改配置描述', json_encode($input), '修改失败');
+            return jsonData(403, '描述修改失败');
+        } catch (\Exception $e) {
+//            var_dump($e->getMessage());
+            AdminLogModel::writeLog($guy, '修改配置描述', json_encode($input), '服务内部错误~');
+            return jsonData(301, '服务内部错误~');
+        }
+    }
+
+    /**修改配置值
+     * @param $input
+     * @return array
+     */
+    public function editValue($input)
+    {
+        $guy = Session::get('info.admin_account');
+        try {
+            //查询配置当前状态
+            $info = Db::table($this->table)->field('config_value')->where('config_id', $input['config_id'])->find();
+            if (null == $info) {
+                return jsonData(401, '未找到当前配置');
+            }
+            if ($input['config_value'] == $info['config_value']) {
+                return jsonData(402, '修改前与当前一致，不能修改');
+            }
+            $rtn = Db::table($this->table)->limit(1)->update($input);
+            if ($rtn == 1) {
+                AdminLogModel::writeLog($guy, '修改配置值', json_encode($input), '修改成功');
+                return jsonData(200, '配置值修改成功');
+            }
+            AdminLogModel::writeLog($guy, '修改配置值', json_encode($input), '修改失败');
+            return jsonData(403, '配置值修改失败');
+        } catch (\Exception $e) {
+//            var_dump($e->getMessage());
+            AdminLogModel::writeLog($guy, '修改配置值', json_encode($input), '服务内部错误~');
+            return jsonData(301, '服务内部错误~');
+        }
+    }
+
+    /**获取Icon
+     * @param $input
+     * @return array
+     */
+    public function getIcon($input)
+    {
+        try {
+            //查询产品当前状态
+            $info = Db::table($this->table)->field('config_value')->where('config_id', $input['config_id'])->find();
+            if (null == $info) {
+                return jsonData(401, '未找到当前icon');
+            }
+
+            return jsonData(200, '获取成功', $info);
+        } catch (\Exception $e) {
+            return jsonData(301, '服务内部错误~');
+        }
+    }
+
+    /**更新Icon
+     * @param $input
+     * @param $icon
+     * @return bool
+     */
+    public function updateIcon($input, $icon)
+    {
+        $guy = Session::get('info.admin_account');
+        try {
+            $data['config_id']    = $input['config_id'];
+            $data['config_value'] = $icon;
+
+            $rtn = Db::table($this->table)->update($data);
+            if ($rtn == 1) {
+                AdminLogModel::writeLog($guy, '更新Icon', json_encode(array_merge($input, ['icon' => $icon])),
+                    '更新成功');
+                return true;
+            }
+            AdminLogModel::writeLog($guy, '更新Icon', json_encode($input), '更新失败');
+            return false;
+        } catch (\Exception $e) {
+            AdminLogModel::writeLog($guy, '更新Icon', json_encode($input), '服务内部错误~');
+            return false;
         }
     }
 }
